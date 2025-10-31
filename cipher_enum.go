4b8f88df632d4f167cf17405791d0b1380c39939
Lord of Scripts ™ <lordofscripts@users.noreply.github.com>
2025-10-31 14:16:52 -0500

    #20 needed for User Profiles

--- cipher_enum.go
+++ cipher_enum.go
@@ -2,11 +2,16 @@
  *					L o r d  O f   S c r i p t s (tm)
  *				  Copyright (C)2025 Dídimo Grimaldo T.
  * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
- * Cipher algorithm enumeration
+ * Cipher algorithm enumeration with YAML (un)marshalling.
  *-----------------------------------------------------------------*/
 package caesarx
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+
+	"gopkg.in/yaml.v3"
+)
 
 /* ----------------------------------------------------------------
  *							G l o b a l s
@@ -21,6 +26,17 @@ const (
 	VigenereCipher
 	AffineCipher
 )
+
+/* ----------------------------------------------------------------
+ *						I n t e r f a c e s
+ *-----------------------------------------------------------------*/
+
+var _ yaml.Unmarshaler = (*CipherVariant)(nil)
+var _ yaml.Marshaler = NoCipher
+
+/* ----------------------------------------------------------------
+ *							L o c a l s
+ *-----------------------------------------------------------------*/
 
 var cipherToString = map[CipherVariant]string{
 	NoCipher:        "None",
@@ -70,11 +86,53 @@ func (c CipherVariant) Convert(v int) (CipherVariant, string, error) {
 }
 
 // Parse takes a cipher name string and attempts to parse it to
-// convert it to the CipherVariant enumeration.
+// convert it to the CipherVariant enumeration. Case insensitive.
 func (c CipherVariant) Parse(str string) (CipherVariant, error) {
-	if v, ok := stringToCipher[str]; !ok {
-		return NoCipher, fmt.Errorf("invalid cipher name '%s'", str)
-	} else {
-		return v, nil
+	var err error = nil
+	var v CipherVariant = NoCipher
+	var ok bool
+
+	if v, ok = stringToCipher[str]; !ok {
+		// try case-insensitive map key search
+		found := false
+		for k, val := range stringToCipher {
+			if strings.EqualFold(k, str) {
+				v = val
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			err = fmt.Errorf("invalid cipher name '%s'", str)
+		}
 	}
+
+	return v, err
+}
+
+// Custom YAML unmarshalling of enumeration string to its numeric value.
+func (c *CipherVariant) UnmarshalYAML(value *yaml.Node) error {
+	// it should be a string
+	var name string
+	if err := value.Decode(&name); err != nil {
+		return err
+	}
+
+	// it was a string, continue
+	if v, ok := stringToCipher[name]; !ok {
+		return fmt.Errorf("parse literal has invalid CipherVariant '%s'", name)
+	} else {
+		*c = v
+		return nil
+	}
+}
+
+// Custom YAML marshalling of enumeration, otherwise it appears as integer.
+func (c CipherVariant) MarshalYAML() (interface{}, error) {
+	return cipherToString[c], nil
+}
+
+func MaxCipher() int {
+	return int(AffineCipher)
 }
